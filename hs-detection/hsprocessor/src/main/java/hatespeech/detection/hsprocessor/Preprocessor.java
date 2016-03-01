40ae15d7652d276756f543e0f94a520eaa51d0fb
Walla1 <klaus.walla@aon.at>
2016-03-01 17:36:28 +0100

    + Added Shingles Function in Preprocessor

--- hs-detection/hsprocessor/src/main/java/hatespeech/detection/hsprocessor/Preprocessor.java
+++ hs-detection/hsprocessor/src/main/java/hatespeech/detection/hsprocessor/Preprocessor.java
@@ -9,24 +9,45 @@ import java.io.StringReader;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.de.GermanAnalyzer;
+import org.apache.lucene.analysis.shingle.ShingleFilter;
+import org.apache.lucene.analysis.standard.StandardFilter;
+import org.apache.lucene.analysis.standard.StandardTokenizer;
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.util.Version;
 
 public class Preprocessor {
 	
 	@SuppressWarnings("resource")
-	public static void LucPreprocessor(String Hassposttext) throws IOException{
+	public static void lucPreprocessor(String hassposttext) throws IOException{
 		Analyzer analyzer = new GermanAnalyzer();
-	    TokenStream tokenStream = analyzer.tokenStream("content", new StringReader(Hassposttext));
+	    TokenStream tokenStream = analyzer.tokenStream("content", new StringReader(hassposttext));
 	    CharTermAttribute charTermAttribute = tokenStream.addAttribute(CharTermAttribute.class);
 	    
 	    tokenStream.reset();
-	    
-	    while(tokenStream.incrementToken())
-	    {
-	    	System.out.println(charTermAttribute.toString());
+	    while (tokenStream.incrementToken()) {
+	        System.out.println(charTermAttribute.toString());
 	    }
 	    tokenStream.end();
 	    tokenStream.close();
+	    
+	    //Generate Bigrams
+	    StandardTokenizer tokenizer = new StandardTokenizer();
+	    tokenizer.setReader(new StringReader(hassposttext));
+	    TokenStream tokenStream2 = new StandardFilter(tokenizer);
+	    CharTermAttribute charTermAttribute2 = tokenStream2.addAttribute(CharTermAttribute.class);
+
+	    ShingleFilter shfilter = new ShingleFilter(tokenStream2,2,3);
+	    shfilter.setOutputUnigrams(true);
+	    shfilter.setOutputUnigramsIfNoShingles(true);
+	    
+	    shfilter.reset();
+	    
+	    while(shfilter.incrementToken())
+	    {
+	    	System.out.println(charTermAttribute2.toString());
+	    }
+	    shfilter.end();
+	    shfilter.close();
 	}
 	
 	public static void main(String[] args){
@@ -35,7 +56,7 @@ public class Preprocessor {
 		try {
 			for(HatePost hatePost: dao.selectAllPosts())
 			{
-				LucPreprocessor(hatePost.getPost());
+				lucPreprocessor(hatePost.getPost());
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
