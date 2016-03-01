90c5b3a52f320d6dbc16e88ab46270170bc6d62a
Walla1 <klaus.walla@aon.at>
2016-03-01 17:19:38 +0100

    + Modified CreateTable

--- hs-detection/service/src/main/java/hatespeech/detection/dao/CreateTable.java
+++ hs-detection/service/src/main/java/hatespeech/detection/dao/CreateTable.java
@@ -10,15 +10,23 @@ public class CreateTable {
 	public static void CreateHSTable() {
 	 try {
 	
-	      String createTableSQL = "CREATE TABLE if not exists HatePost " +
-	                   "(ID INTEGER PRIMARY KEY  AUTOINCREMENT," +
-	                   " InternID   TEXT   UNIQUE, " + 
-	                   " Post       TEXT NOT NULL UNIQUE, " + 
-	                   " Link      TEXT)"; 
-	      
-	      PreparedStatement  preStat = DatabaseConnector.getConnection().prepareStatement(createTableSQL);
+			String createHPTableSQL = "CREATE TABLE if not exists HatePost "
+					+ "(ID INTEGER PRIMARY KEY  AUTOINCREMENT,"
+					+ " InternID   TEXT   UNIQUE, "
+					+ " Post       TEXT NOT NULL UNIQUE, "
+					+ " Link      TEXT);";
 
+	      PreparedStatement  preStat = DatabaseConnector.getConnection().prepareStatement(createHPTableSQL);
 	      preStat.executeUpdate();
+	      
+	      String createFBPTableSQL="CREATE TABLE if not exists FBPost (id TEXT PRIMARY KEY, commentsCount INTEGER, createdTime TEXT, fromId TEXT, likesCount INTEGER, message TEXT, sharesCount INTEGER, type TEXT);";
+	      preStat = DatabaseConnector.getConnection().prepareStatement(createFBPTableSQL);
+	      preStat.executeUpdate();
+	      
+	      String createFBCTableSQL="CREATE TABLE if not exists FBComment (id TEXT PRIMARY KEY, postId TEXT, createdTime TEXT, commentCount INTEGER, fromId TEXT, likeCount INTEGER, message TEXT, parentId TEXT, FOREIGN KEY (postId) REFERENCES FBPost (id), FOREIGN KEY (parentId) REFERENCES FBComment (id));";
+	      preStat = DatabaseConnector.getConnection().prepareStatement(createFBCTableSQL);
+	      preStat.executeUpdate();
+	      
 	      preStat.close();
 	      System.out.println("Table created successfully");
 	      
