23e5bd30deff5379129886762273528d7d28fc31
Walla1 <klaus.walla@aon.at>
2016-03-01 15:46:34 +0100

    + Added Method LucenePreprocessor + Added Pom-Dependecy (Lucene Analyzer)

--- hs-detection/hsprocessor/src/main/java/hatespeech/detection/hsprocessor/Preprocessor.java
+++ hs-detection/hsprocessor/src/main/java/hatespeech/detection/hsprocessor/Preprocessor.java
@@ -1,5 +1,40 @@
 package hatespeech.detection.hsprocessor;
 
-public class Preprocessor {
+import java.io.IOException;
+import java.io.StringReader;
 
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.de.GermanAnalyzer;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+
+public class Preprocessor {
+	
+	@SuppressWarnings("resource")
+	public static void LucPreprocessor() throws IOException{
+		Analyzer analyzer = new GermanAnalyzer();
+	    TokenStream tokenStream = analyzer.tokenStream("content", new StringReader("Hass-posting: Das ist ein schlimmer Hasspost"));
+	    CharTermAttribute charTermAttribute = tokenStream.addAttribute(CharTermAttribute.class);
+	    
+	    tokenStream.reset();
+	    
+	    while(tokenStream.incrementToken())
+	    {
+	    	System.out.println(charTermAttribute.toString());
+	    }
+	    tokenStream.end();
+	    tokenStream.close();
+	}
+	
+	public static void main(String[] args){
+		try {
+			LucPreprocessor();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+    
 }
+    
