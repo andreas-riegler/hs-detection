7c46377cac8cbd8a42b305aea72d3df666109bc4
Andreas Riegler <andreas-riegler@hotmail.com>
2016-03-08 23:58:29 +0100

    removed split

--- hs-detection/hsprocessor/src/main/java/hatespeech/detection/ml/WekaBowClassifier.java
+++ hs-detection/hsprocessor/src/main/java/hatespeech/detection/ml/WekaBowClassifier.java
@@ -31,38 +31,38 @@ public class WekaBowClassifier {
 	private Classifier classifier;
 	private FilteredClassifier filteredClassifier;
 	public WekaBowClassifier(List<Posting> trainingSamples,Classifier classifier){
-		
+
 		this.classifier=classifier;
 		trainingInstances=initializeInstances("train",trainingSamples);
-		
+
 		filter=initializeBOWFilter();
 		filter.setAttributeIndices("first");
-		
+
 	}
-	
-	
+
+
 
 	private Instances initializeInstances(String name,List<Posting> trainingSamples) {
-		
+
 		ArrayList<Attribute> featureList=new ArrayList<Attribute>();
 		featureList.add(new Attribute("message",(List<String>)null));
-		
+
 		List<String> hatepostResults = new ArrayList<String>();
 		hatepostResults.add("negative");
 		hatepostResults.add("positive");
 		featureList.add(new Attribute("__hatepost__",hatepostResults));
-		
-		
+
+
 		Instances instances = new Instances(name, featureList, trainingSamples.size());
 		instances.setClassIndex(featureList.size()-1);
-		
+
 		updateData(trainingSamples, instances, featureList.size());
-		
+
 		return instances;
 	}
-	
+
 	private void updateData(List<Posting> trainingSamples, Instances instances,int rowSize) {
-		
+
 		for(Posting post:trainingSamples)
 		{
 			DenseInstance instance = createInstance(post.getMessage(),instances,rowSize);
@@ -70,40 +70,40 @@ public class WekaBowClassifier {
 			instances.add(instance);
 		}	
 	}
-	
-	 /**
-	   * Method that converts a text message into an instance.
-	   */
-	 private DenseInstance createInstance(String text, Instances data,int rowSize) {
 
-	    // Create instance of length rowSize
-	    DenseInstance instance = new DenseInstance(rowSize);
+	/**
+	 * Method that converts a text message into an instance.
+	 */
+	private DenseInstance createInstance(String text, Instances data,int rowSize) {
 
-	    // Give instance access to attribute information from the dataset.
-	    instance.setDataset(data);
-	    
-	    // Set value for message attribute
-	    Attribute messageAtt = data.attribute("message");
-	    instance.setValue(messageAtt, text);
+		// Create instance of length rowSize
+		DenseInstance instance = new DenseInstance(rowSize);
 
-	    
-	    return instance;
-	  }
-	
+		// Give instance access to attribute information from the dataset.
+		instance.setDataset(data);
+
+		// Set value for message attribute
+		Attribute messageAtt = data.attribute("message");
+		instance.setValue(messageAtt, text);
+
+
+		return instance;
+	}
+
 	private StringToWordVector initializeBOWFilter() {
 		NGramTokenizer tokenizer = new NGramTokenizer();
 		tokenizer.setNGramMinSize(1);
-		tokenizer.setNGramMaxSize(2);
+		tokenizer.setNGramMaxSize(1);
 		tokenizer.setDelimiters("\\W");
-		
+
 		StringToWordVector filter = new StringToWordVector();
 		//filter.setInputFormat(trainingInstances);
-		
+
 		filter.setTokenizer(tokenizer);
 		filter.setWordsToKeep(1000000);
 		//filter.setDoNotOperateOnPerClassBasis(true);
 		filter.setLowerCaseTokens(true);
-		
+
 		return filter;
 	}
 
@@ -114,7 +114,7 @@ public class WekaBowClassifier {
 	 */
 	public void evaluate() {
 		try {
-			
+
 			filteredClassifier = new FilteredClassifier();
 			filteredClassifier.setFilter(filter);
 			filteredClassifier.setClassifier(classifier);
@@ -129,7 +129,7 @@ public class WekaBowClassifier {
 			System.out.println("Problem found when evaluating");
 		}
 	}
-	
+
 	/**
 	 * This method trains the classifier on the dataset.
 	 */
@@ -147,54 +147,38 @@ public class WekaBowClassifier {
 			System.out.println("Problem found when training");
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		JDBCFBCommentDAO daoFB= new JDBCFBCommentDAO();
 		JDBCHSPostDAO daoHP= new JDBCHSPostDAO();
-		
+
 		List<Posting> trainingSamples = new ArrayList<Posting>();
 		List<Posting> testSamples=new ArrayList<Posting>();
-		
-			
-			double i=0;
-			for(FBComment post: daoFB.getFBComments())
+
+		for(FBComment post: daoFB.getFBComments())
+		{
+			if(post.getResult()!=-1)
 			{
-				if(post.getResult()!=-1)
+				if(post.getResult()==0)
 				{
-					if(post.getResult()==0)
-					{
-						if((i/314.0)<=0.8)
-							trainingSamples.add(new Posting(post.getMessage(),PostType.NEGATIVE));
-						else
-							testSamples.add(new Posting(post.getMessage(),PostType.NEGATIVE));
-						
-					}	
-					else if(post.getResult()==1)
-					{
-						if((i/314.0)<=0.8)
-							trainingSamples.add(new Posting(post.getMessage(),PostType.POSITIVE));
-						else
-							testSamples.add(new Posting(post.getMessage(),PostType.POSITIVE));
-					}
-					i++;
+					trainingSamples.add(new Posting(post.getMessage(),PostType.NEGATIVE));				
+				}	
+				else if(post.getResult()==1)
+				{
+					trainingSamples.add(new Posting(post.getMessage(),PostType.POSITIVE));
 				}
-					
 			}
-			i=0;
-			
-			for(HatePost hatePost: daoHP.getAllPosts())
-			{
-				if(i<=300){
-				if((i/300.0)<=0.8)
-					trainingSamples.add(new Posting(hatePost.getPost(),PostType.POSITIVE));
-				else
-					testSamples.add(new Posting(hatePost.getPost(),PostType.POSITIVE));
-				}
-				i++;
-			}
-			WekaBowClassifier classifier=new WekaBowClassifier(trainingSamples,new SMO());
-			classifier.evaluate();
-			classifier.learn();
+
+		}
+
+		for(HatePost hatePost: daoHP.getAllPosts())
+		{
+			trainingSamples.add(new Posting(hatePost.getPost(),PostType.POSITIVE));
+		}
+
+		WekaBowClassifier classifier=new WekaBowClassifier(trainingSamples,new SMO());
+		classifier.evaluate();
+		classifier.learn();
 	}
 
 }
