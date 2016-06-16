cf828a2f1cf4a6f8060f4bc952ac5e80790ebe16
Walla1 <klaus.walla@aon.at>
2016-06-16 12:55:04 +0200

    +Added Classifier Main-methodes

---
+++ hs-detection/hsprocessor/src/main/java/hatespeech/detection/main/ClassifierTwitter.java
@@ -0,0 +1,59 @@
+package hatespeech.detection.main;
+
+import hatespeech.detection.dao.JDBCFBCommentDAO;
+import hatespeech.detection.dao.JDBCHSPostDAO;
+import hatespeech.detection.ml.WekaBowClassifier;
+import hatespeech.detection.model.FBComment;
+import hatespeech.detection.model.HatePost;
+import hatespeech.detection.model.PostType;
+import hatespeech.detection.model.Posting;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import weka.classifiers.functions.SMO;
+
+public class ClassifierTwitter {
+
+	public static void main(String[] args) {
+		JDBCFBCommentDAO daoFB= new JDBCFBCommentDAO();
+		JDBCHSPostDAO daoHP= new JDBCHSPostDAO();
+
+		List<Posting> trainingSamples = new ArrayList<Posting>();
+
+		for(FBComment post: daoFB.getFBComments())
+		{
+			if(post.getResult()!=-1)
+			{
+				if(post.getResult()==0)
+				{
+					trainingSamples.add(new Posting(post.getMessage(), post.getTypedDependencies(), PostType.NEGATIVE));				
+				}	
+				else if(post.getResult() == 1)
+				{
+					trainingSamples.add(new Posting(post.getMessage(), post.getTypedDependencies(), PostType.POSITIVE));
+				}
+			}
+
+		}
+
+		for(HatePost hatePost: daoHP.getAllPosts())
+		{
+			if(hatePost.getResult() == 1){
+				trainingSamples.add(new Posting(hatePost.getPost(), hatePost.getTypedDependencies(), PostType.POSITIVE));
+			}
+		}
+
+		WekaBowClassifier classifier1 = new WekaBowClassifier(trainingSamples, new SMO());
+		classifier1.evaluate();
+
+		//WekaBowClassifier classifier2 = new WekaBowClassifier(trainingSamples, new SMO());
+		//classifier2.setMessageExactMatch(false);
+		//classifier2.evaluate();
+
+		//classifier1.learn();
+
+		//classifier1.findFalsePositives(5);
+	}
+
+}
